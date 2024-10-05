df29723bbc057775377081dab7b1d7665cc4afdd
Celine Natasha Baez Avila <138732290+ninebaez@users.noreply.github.com>
2024-10-04 19:25:56 -0500

    nuevos cambios
    se agregaron nuevos scripts.

---
+++ Primer video/Assets/scripts/CameraLook.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+  public float mousensitivity = 80f;
+
+  public Transform playerbody;
+
+  float xRotation = 0;
+
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+       float mouseX = Input.GetAxis("Mouse X") * mousensitivity * Time.deltaTime;
+      
+       float mouseY = Input.GetAxis("Mouse Y") * mousensitivity * Time.deltaTime;
+
+
+       xRotation -= mouseY;
+
+       xRotation=Mathf.Clamp(xRotation, -90f,90);
+
+       transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+
+       playerbody.Rotate(Vector3.up*mouseX);
+
+    }
+}
