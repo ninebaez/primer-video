8a4bbd15b7fdc0f46b9f64b944fdb2b15abd4f88
Celine Natasha Baez Avila <138732290+ninebaez@users.noreply.github.com>
2024-10-05 11:57:21 -0500

    Update CameraLook.cs

--- Primer video/Assets/scripts/CameraLook.cs
+++ Primer video/Assets/scripts/CameraLook.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraLook : MonoBehaviour
 {
     
   public float mousensitivity = 80f;
